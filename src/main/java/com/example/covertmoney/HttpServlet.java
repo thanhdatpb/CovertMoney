8575ac5be04b15c5f26f542187fe4d800a04a305
Thanh Dat <tthanhdat.pb@gmail.com>
2024-06-13 17:05:16 +0700

    clean code

--- src/main/java/com/example/covertmoney/HttpServlet.java
+++ src/main/java/com/example/covertmoney/HttpServlet.java
@@ -9,27 +9,21 @@ public abstract class HttpServlet {
     }
 
     protected void doGet(Object response)
-                throws ServletException, IOException, Convert.ServletException, InterruptedException {
+            throws ServletException, IOException, Convert.ServletException, InterruptedException {
         doGet(null, response);
     }
 
     protected abstract void doGet(Object request, Object response)
-                throws ServletException, IOException, Convert.ServletException, InterruptedException;
+            throws ServletException, IOException, Convert.ServletException, InterruptedException;
     protected void doPost()
             throws ServletException, IOException, Convert.ServletException, InterruptedException {
         doPost(null, null);
     }
 
-//        /**
-//         * @throws ServletException
-//         * @throws IOException
-//         * @throws Convert.ServletException
-//         * @throws InterruptedException
-//         */
-        protected void doPost(Object request)
-                throws ServletException, IOException, Convert.ServletException, InterruptedException {
+    protected void doPost(Object request)
+            throws ServletException, IOException, Convert.ServletException, InterruptedException {
             doPost(request, null);
-        }
+    }
 
     protected abstract void doPost(Object request, Object response)
             throws ServletException, IOException, Convert.ServletException, InterruptedException;
